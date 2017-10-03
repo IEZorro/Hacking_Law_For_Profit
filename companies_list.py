99b951ca0337a45d22967522fdae33d0fc68fa87
Sar <estevezisauro@gmail.com>
2017-10-03 10:53:13 -0700

    fixed bs, use python3 in cmd

--- companies_list.py
+++ companies_list.py
@@ -1,6 +1,5 @@
-import bs4 as bs
-from urlib2 import urlopen
-
-sauce = urllib.request.urlopen('https://www.sec.gov/rules/other/4-460list.htm').read()
-soup = bs.BeautifulSoup(sauce, 'lxml')
-print(soup)
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+html = urlopen("https://www.sec.gov/rules/other/4-460list.htm")
+bsObj = BeautifulSoup(html.read(), 'lxml')
+print(bsObj.td)
