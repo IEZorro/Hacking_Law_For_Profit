b577e9a1a74b387af4c6a9c45fdcf836fc302b36
IEZorro <estevezisauro@gmail.com>
2017-10-07 22:15:00 -0700

    Scraped Company list to txt file & began template for Regex Parser kinda

--- companies_list.py
+++ companies_list.py
@@ -2,16 +2,20 @@
 #beginning of the page and at the end. Assuming companies don't dissapear, we could
 #have it start appending to list once it hits the first company name and stop at
 #the final one.
+# https://www.sec.gov/rules/other/4-460list.htm
 
-# Another problem is that this output is being treated as individual characters and not strings found in a tag.
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-html = urlopen("https://www.sec.gov/rules/other/4-460list.htm")
-bsObj = BeautifulSoup(html.read(), 'lxml')
-print(html)
-print(len(bsObj.text))
+#Took Forever
+
+#import necessary modules
+import requests, bs4
+
+#call the webpage
+res = requests.get('https://www.sec.gov/rules/other/4-460list.htm')
+
+noStarchSoup = bs4.BeautifulSoup(res.text, 'lxml')
 
 
 
-import pdfkit
-pdfkit.from_url('http://google.com', 'out.pdf')
+#Sends the Text of the web page to the txt file in the txt folder 
+writing = open('txt/companies_list.txt', 'w')
+writing.write(noStarchSoup.text)
--- text_parse.py
+++ text_parse.py
@@ -1,5 +1,5 @@
 #Making it so that our for loop goes through lines themselves and not individual characters so that we may parse easily using Regex.
 create = open('txt/pdf_txt/DataScience.pdf.txt', 'r')
 for line in create:
-    #Regex Here
+    #Regex Here: Looking for company names which we will have to scrape from the other site.
     print(line)
